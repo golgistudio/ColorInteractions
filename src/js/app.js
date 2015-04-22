610ac70a9836c0c6872607f89efe5a70664cb895
golgistudio <laurie.reynolds@golgistudio.com>
2015-04-21 17:44:31 -0700

    Refactored to eliminate duplicate code

--- src/js/app.js
+++ src/js/app.js
@@ -2,50 +2,139 @@
     var $container = $('.offcanvas-bottom'),
         $cHeight = $('.o-content').outerHeight();
 
+    var colorWidgetA;
+    var colorWidgetB;
+
     $(document).ready(function() {
         buildCanvas();
 
-        InitializeColorSpectrums();
+        InitializeColorWidgetA();
+        InitializeColorWidgetB();
 
-        var flatSpectrumElement =  $("#flatSpectrum");
+        InitializeBackgroundSpectrum();
 
-        flatSpectrumElement.spectrum({
-            flat: true,
-            showInput: true,
-            showPalette: true,
-            showButtons: false,
-            clickoutFiresChange: true
-        });
-
-        handleHexChangeA("#ffd238", "1.0");
-        handleHexChangeB("#819bff", "1.0");
-        updateBackground("#DDDDDD");
-
-        colorAEventHandlers();
-        colorBEventHandlers();
-
-        var textElement = $("#sampleText");
-        var textValue = textElement.val();
-        updatePanelText(textValue);
-
-        textElement.on("change", function() {
-            var textValue = $("#sampleText").val();
-            updatePanelText(textValue);
-        }  ) ;
+        InitializeTextWidget();
 
         // Recalculate after adding color pickers
         $cHeight = $('.o-content').outerHeight();
 
-        flatSpectrumElement.on('move.spectrum', function(e, tinyColor) {
-            updateBackgroundNoSpectrum(tinyColor);
-        });
-
+        // Background image event handler
         $("#imgInp").change(function(){
             readURL(this);
         });
 
 
     });
+
+
+    function  InitializeColorWidgetA() {
+        colorWidgetA = new ColorWidget();
+        colorWidgetA.name = "colorA";
+        colorWidgetA.hex = document.getElementById('hex_a');
+        colorWidgetA.red = document.getElementById('rgb_r_a');
+        colorWidgetA.green = document.getElementById('rgb_g_a');
+        colorWidgetA.blue = document.getElementById('rgb_b_a');
+        colorWidgetA.opacity = document.getElementById('opacity_a');
+        colorWidgetA.colorSwatch = document.getElementById('colorPickerA');
+        colorWidgetA.spectrum = $("#flatSpectrum_a");
+
+        // Initialize spectrum
+        InitializeColorSpectrum(colorWidgetA);
+
+        // Set initial color
+        colorWidgetA.hex.value = "#ffd238" ;
+        colorWidgetA.opacity.value = "1.0";
+        handleHexChange(colorWidgetA);
+
+        // Set up event handlers
+        colorEventHandlerA();
+    }
+
+    function  InitializeColorWidgetB() {
+        colorWidgetB = new ColorWidget();
+        colorWidgetB.name = "colorB";
+        colorWidgetB.hex = document.getElementById('hex_b');
+        colorWidgetB.red = document.getElementById('rgb_r_b');
+        colorWidgetB.green = document.getElementById('rgb_g_b');
+        colorWidgetB.blue = document.getElementById('rgb_b_b');
+        colorWidgetB.opacity = document.getElementById('opacity_b');
+        colorWidgetB.colorSwatch = document.getElementById('colorPickerB');
+        colorWidgetB.spectrum = $("#flatSpectrum_b");
+
+        // Initialize spectrum
+        InitializeColorSpectrum(colorWidgetB);
+
+        // Set initial color
+        colorWidgetB.hex.value = "#819bff" ;
+        colorWidgetB.opacity.value = "1.0";
+
+        handleHexChange(colorWidgetB);
+
+        // Set up event handlers
+        colorEventHandlerB();
+    }
+
+
+    function InitializeColorSpectrum(colorWidget) {
+        colorWidget.spectrum.spectrum({
+            flat: true,
+            showAlpha: true,
+            allowAlpha: true,
+            showPalette: true,
+            showButtons: false,
+            clickoutFiresChange: true
+        });
+
+    }
+
+    function colorEventHandlerA() {
+
+        colorWidgetA.hex.onchange = function () {
+            handleHexChange(colorWidgetA);
+        };
+
+        colorWidgetA.red.onchange = function () {
+            handleRGBAChange(colorWidgetA);
+        };
+        colorWidgetA.green.onchange = function () {
+            handleRGBAChange(colorWidgetA);
+        };
+        colorWidgetA.blue.onchange = function () {
+            handleRGBAChange(colorWidgetA);
+        };
+        colorWidgetA.opacity.onchange = function () {
+            handleRGBAChange(colorWidgetA);
+        };
+
+        colorWidgetA.spectrum.on('move.spectrum', function (e, tinyColor) {
+            spectrumUpdate(tinyColor, colorWidgetA);
+        });
+    }
+
+    function colorEventHandlerB() {
+
+        colorWidgetB.hex.onchange =  function() {
+            handleHexChange(colorWidgetB);
+        };
+
+        colorWidgetB.red.onchange = function() {
+            handleRGBAChange(colorWidgetB);
+        };
+        colorWidgetB.green.onchange =  function() {
+            handleRGBAChange( colorWidgetB);
+        };
+        colorWidgetB.blue.onchange =  function() {
+            handleRGBAChange(colorWidgetB);
+        };
+        colorWidgetB.opacity.onchange =  function() {
+            handleRGBAChange(colorWidgetB);
+        };
+
+        colorWidgetB.spectrum.on('move.spectrum', function(e, tinyColor) {
+            spectrumUpdate(tinyColor, colorWidgetB);
+        });
+
+    }
 
     function readURL(input) {
         if (input.files && input.files[0]) {
@@ -61,6 +150,24 @@
         }
     }
 
+    function InitializeBackgroundSpectrum() {
+        var flatSpectrumElement =  $("#flatSpectrum");
+
+        flatSpectrumElement.spectrum({
+            flat: true,
+            showInput: true,
+            showPalette: true,
+            showButtons: false,
+            clickoutFiresChange: true
+        });
+
+        updateBackground("#DDDDDD");
+
+        flatSpectrumElement.on('move.spectrum', function(e, tinyColor) {
+            updateBackgroundNoSpectrum(tinyColor);
+        });
+    }
+
     function updateBackground(hexVal) {
         updateBackgroundNoSpectrum( hexVal);
         $("#flatSpectrum").spectrum("set", hexVal);
@@ -70,6 +177,18 @@
         $("#containerBackground").css('background', hexVal);
         var backgroundColor = document.getElementById('colorPickerBackground');
         backgroundColor.style.backgroundColor = hexVal;
+    }
+
+    function InitializeTextWidget() {
+
+        var textElement = $("#sampleText");
+        var textValue = textElement.val();
+        updatePanelText(textValue);
+
+        textElement.on("change", function () {
+            var textValue = $("#sampleText").val();
+            updatePanelText(textValue);
+        });
     }
 
     function updatePanelText(textValue) {
--- src/js/colorManager.js
+++ src/js/colorManager.js
@@ -1,202 +1,101 @@
 
 
-var iHex_a = document.getElementById('hex_a');
-var iR_a = document.getElementById('rgb_r_a');
-var iG_a = document.getElementById('rgb_g_a');
-var iB_a = document.getElementById('rgb_b_a');
-var iO_a = document.getElementById('opacity_a');
-
-var color_a = document.getElementById('colorPickerA');
-
-var iHex_b = document.getElementById('hex_b');
-var iR_b = document.getElementById('rgb_r_b');
-var iG_b = document.getElementById('rgb_g_b');
-var iB_b = document.getElementById('rgb_b_b');
-var iO_b = document.getElementById('opacity_b');
-
-var spectrumColor_A;
-var spectrumColor_B;
-
-var color_b = document.getElementById('colorPickerB');
-
-function InitializeColorSpectrums() {
-    spectrumColor_A = $("#flatSpectrum_a").spectrum({
-        flat: true,
-        showAlpha: true,
-        allowAlpha: true,
-        showPalette: true,
-        showButtons: false,
-        clickoutFiresChange: true
-    });
-
-    spectrumColor_B = $("#flatSpectrum_b").spectrum({
-        flat: true,
-        showAlpha: true ,
-        allowAlpha: true,
-        showPalette: true,
-        showButtons: false,
-        clickoutFiresChange: true
-    });
-}
-
-function colorAEventHandlers() {
-
-    iHex_a.onchange = function() {
-        handleHexChangeA(iHex_a.value, iO_a.value);
-    };
-
-    iR_a.onchange = function() {
-        handleRGBAChangeA(Math.round(iR_a.value), Math.round(iG_a.value), Math.round(iB_a.value), iO_a.value);
-    };
-    iG_a.onchange = function() {
-        handleRGBAChangeA(Math.round(iR_a.value), Math.round(iG_a.value), Math.round(iB_a.value), iO_a.value);
-    };
-    iB_a.onchange = function() {
-        handleRGBAChangeA(Math.round(iR_a.value), Math.round(iG_a.value), Math.round(iB_a.value), iO_a.value);
-    };
-    iO_a.onchange = function() {
-        handleRGBAChangeA(Math.round(iR_a.value), Math.round( iG_a.value), Math.round(iB_a.value), iO_a.value);
-    };
-
-    $("#flatSpectrum_a").on('move.spectrum', function(e, tinyColor) {
-        spectrumAUpdate(tinyColor);
-    });
-
-}
-
-function colorBEventHandlers() {
-
-    iHex_b.onchange = function() {
-        handleHexChangeB(iHex_b.value, iO_b.value);
-    };
-
-    iR_b.onchange = function() {
-        handleRGBAChangeB(Math.round(iR_b.value), Math.round(iG_b.value), Math.round(iB_b.value), iO_b.value);
-    };
-    iG_b.onchange = function() {
-        handleRGBAChangeB(Math.round(iR_b.value), Math.round(iG_b.value), Math.round(iB_b.value), iO_b.value);
-    };
-    iB_b.onchange = function() {
-        handleRGBAChangeB(Math.round(iR_b.value), Math.round(iG_b.value), Math.round(iB_b.value), iO_b.value);
-    };
-    iO_b.onchange = function() {
-        handleRGBAChangeB(Math.round(iR_b.value), Math.round(iG_b.value), Math.round(iB_b.value), iO_b.value);
-    };
-
-    $("#flatSpectrum_b").on('move.spectrum', function(e, tinyColor) {
-        spectrumBUpdate(tinyColor);
-    });
+function ColorWidget () {
+    this.name = null;
+    this.hex = null;
+    this.red = null;
+    this.green = null;
+    this.blue = null;
+    this.opacity = null;
+    this.spectrum = null;
+    this.colorSwatch = null;
+    this.textPanel = null;
+    this.panel1 = null;
+    this.panel2 = null;
+    this.panel3 = null;
 }
 
 
-// Color Control A
-function handleHexChangeA(hexValue, opacityValue) {
-    var newTinyColor = tinycolor(hexValue);
-    newTinyColor._a = opacityValue;
-    updateColorA(newTinyColor);
+function handleHexChange(colorWidgetItem) {
+    var newTinyColor = tinycolor(colorWidgetItem.hex.value);
+    newTinyColor._a = colorWidgetItem.opacity.value;
+    updateColor(newTinyColor, colorWidgetItem);
 }
 
-function handleRGBAChangeA(red, green, blue, opacity) {
-    var newTinyColor = tinycolor({r: red,  g: green, b: blue, a: opacity});
-    updateColorA(newTinyColor);
+function handleRGBAChange(colorWidgetItem) {
+    var newTinyColor = tinycolor({r: colorWidgetItem.red.value,  g: colorWidgetItem.green.value, b: colorWidgetItem.blue.value, a: colorWidgetItem.opacity.value});
+    updateColor(newTinyColor, colorWidgetItem);
 }
 
-function updateColorPickerA(tinyColor) {
-    color_a.style.backgroundColor = tinyColor.toHexString(false);
-    var rgbaVal = "rgba("  + Math.round(tinyColor._r) + ", " + Math.round(tinyColor._g) + ", " + Math.round(tinyColor._b) + ", "  + tinyColor._a + ")";
-    $("#colorPickerA").css('background-color', rgbaVal);
+function spectrumUpdate(tinyColor, colorWidgetItem ) {
+    updateColorNoSpectrum(tinyColor, colorWidgetItem);
+
+}
+function updateColorNoSpectrum(tinyColor, colorWidgetItem) {
+    updateColorPicker(tinyColor, colorWidgetItem);
+    updatePanelsColor(tinyColor, colorWidgetItem);
+    updateInputs(tinyColor, colorWidgetItem);
 }
 
-function updatePanelsColorA(tinyColor) {
+function updateColor(tinyColor, colorWidgetItem) {
+    updateColorNoSpectrum(tinyColor, colorWidgetItem);
+    updateSpectrum(tinyColor, colorWidgetItem);
+}
+
+// only set the values if they are different
+function updateInputs(tinyColor, colorWidgetItem) {
+
+    var hexVal =   tinyColor.toHexString(false);
+    if (hexVal !== colorWidgetItem.hex.value) {
+        colorWidgetItem.hex.value = hexVal;
+    }
+
+    var colorVal =  Math.round(tinyColor._r);
+    if (colorVal !== colorWidgetItem.red.value ) {
+        colorWidgetItem.red.value = colorVal;
+    }
+
+    var colorVal =  Math.round(tinyColor._g);
+    if (colorVal !== colorWidgetItem.green.value ) {
+        colorWidgetItem.green.value = colorVal;
+    }
+
+    var colorVal =  Math.round(tinyColor._b);
+    if (colorVal !== colorWidgetItem.blue.value ) {
+        colorWidgetItem.blue.value = colorVal;
+    }
+
+    var colorVal =  tinyColor._a;
+    if (colorVal !== colorWidgetItem.opacity.value ) {
+        colorWidgetItem.opacity.value = colorVal;
+    }
+}
+
+function updateColorPicker(tinyColor, colorWidgetItem) {
+    colorWidgetItem.colorSwatch.style.backgroundColor = "rgba(" + Math.round(tinyColor._r) + ", " + Math.round(tinyColor._g) + ", " + Math.round(tinyColor._b) + ", " + tinyColor._a + ")";
+}
+
+function updatePanelsColor(tinyColor, colorWidgetItem) {
     var rgbaVal = "rgba(" + Math.round(tinyColor._r) + ", " + Math.round(tinyColor._g) + ", " + Math.round(tinyColor._b) + ", " + tinyColor._a + ")";
-    $("#panel_1_inner").css('background-color', rgbaVal);
-    $("#panel_2_outer").css('background-color', rgbaVal);
-    $("#panel_3_inner").css('color', rgbaVal);
-    $("#panel_4_outer").css('background-color', rgbaVal);
+
+    if (colorWidgetItem.name === "colorA") {
+        $("#panel_1_inner").css('background-color', rgbaVal);
+        $("#panel_2_outer").css('background-color', rgbaVal);
+        $("#panel_3_inner").css('color', rgbaVal);
+        $("#panel_4_outer").css('background-color', rgbaVal);
+    } else {
+        $("#panel_1_outer").css('background-color', rgbaVal);
+        $("#panel_2_inner").css('background-color', rgbaVal);
+        $("#panel_3_outer").css('background-color', rgbaVal);
+        $("#panel_4_inner").css('color', rgbaVal);
+    }
 }
 
-function updateSpectrum_A(tinyColor) {
-    spectrumColor_A.spectrum("set", tinyColor);
+function updateSpectrum(tinyColor, colorWidgetItem) {
+    colorWidgetItem.spectrum.spectrum("set", tinyColor);
 }
 
-function spectrumAUpdate(tinyColor ) {
-    updateColorANoSpectrum(tinyColor);
 
-}
-function updateColorANoSpectrum(tinyColor) {
-    updateColorPickerA(tinyColor);
-    updatePanelsColorA(tinyColor);
-    updateInputs_A(tinyColor);
-}
 
-function updateColorA(tinyColor) {
-    updateColorANoSpectrum(tinyColor);
-    updateSpectrum_A(tinyColor);
-}
-
-// Color Control B
-function handleHexChangeB(hexValue, opacityValue) {
-    var newTinyColor = tinycolor(hexValue);
-    newTinyColor._B = opacityValue;
-    updateColorB(newTinyColor);
-}
-
-function handleRGBAChangeB(red, green, blue, opacity) {
-    var newTinyColor = tinycolor({r: red,  g: green, b: blue, a: opacity});
-    updateColorB(newTinyColor);
-}
-
-function updateColorPickerB(tinyColor) {
-    color_b.style.backgroundColor = tinyColor.toHexString(false);
-    var rgbaVal = "rgba("  + Math.round(tinyColor._r) + ", " + Math.round(tinyColor._g) + ", " + Math.round(tinyColor._b) + ", "  + tinyColor._a + ")";
-    $("#colorPickerB").css('background-color', rgbaVal);
-}
-
-function updatePanelsColorB(tinyColor) {
-    var rgbaVal = "rgba("  + Math.round(tinyColor._r) + ", " + Math.round(tinyColor._g) + ", " + Math.round(tinyColor._b) + ", "  + tinyColor._a + ")";
-    $("#panel_1_outer").css('background-color', rgbaVal);
-    $("#panel_2_inner").css('background-color', rgbaVal);
-    $("#panel_3_outer").css('background-color', rgbaVal);
-    $("#panel_4_inner").css('color', rgbaVal);
-}
-
-function updateInputs_A(tinyColor) {
-    iHex_a.value = tinyColor.toHexString(false);
-
-    iR_a.value = Math.round(tinyColor._r);
-    iG_a.value = Math.round(tinyColor._g);
-    iB_a.value = Math.round(tinyColor._b);
-
-    iO_a.value = tinyColor._a;
-}
-
-function updateInputs_B(tinyColor) {
-    iHex_b.value = tinyColor.toHexString(false);
-
-    iR_b.value = Math.round(tinyColor._r);
-    iG_b.value = Math.round(tinyColor._g);
-    iB_b.value = Math.round(tinyColor._b);
-    iO_b.value = tinyColor._a;
-}
-
-function updateSpectrum_B(tinyColor) {
-    spectrumColor_B.spectrum("set", tinyColor);
-
-}
-
-function updateColorB(tinyColor) {
-    updateColorBNoSpectrum(tinyColor);
-    updateSpectrum_B(tinyColor);
-}
-
-function spectrumBUpdate(tinyColor ) {
-    updateColorBNoSpectrum(tinyColor);
-}
-
-function updateColorBNoSpectrum(tinyColor) {
-    updateColorPickerB(tinyColor);
-    updatePanelsColorB(tinyColor);
-    updateInputs_B(tinyColor);
-}
 
 
